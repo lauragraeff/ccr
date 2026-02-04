287238008307d2e9aeb9cc4ed282270025aa2d45
Graefzera <laugraeffcorreia1821@gmail.com>
2026-02-04 17:58:26 -0300

    consertando componentes

--- src/components/layout/Table.tsx
+++ src/components/layout/Table.tsx
@@ -2,18 +2,22 @@ export default function Table() {
   return (
     <>
       <div>
-        <table className="bg-[#1e1e1e]">
+        <table className="bg-[#1e1e1e] mt-7.5 mx-18 text-white montserrat border-spacing-x-6">
           {/* Cabeçalho da tabela */}
           <thead>
-              {/* Título da tabela */}
-            <tr>
-              <th>Tipos de caminhões cadastrados</th>
-              <th>
-                <button>+ Cadastrar caminhão</button>
+            {/* Título da tabela */}
+            <tr className="">
+              <th colSpan={3} className="">
+                <div className="flex justify-between items-center gap-10">
+                  <h2 className="sm:text-[16px] lg:text-[26px] p-4">
+                    Tipos de caminhões cadastrados
+                  </h2>
+                  <button className="p-4">+ Cadastrar caminhão</button>
+                </div>
               </th>
             </tr>
 
-              {/* Títulos colunas tabela */}
+            {/* Títulos colunas tabela */}
             <tr>
               <th>Nome do tipo de caminhão</th>
               <th>Opções</th>
@@ -39,52 +43,51 @@ export default function Table() {
   );
 }
 
+// TABELA CONTAINER
 
-    // TABELA CONTAINER
+// <div className="tabela container">
+// {/* Cabeçalho tabela */}
+// <div>
+//     {/* Título da tabela */}
+//     <p>Tipos de caminhões cadastrados</p>
 
-    // <div className="tabela container">
-    // {/* Cabeçalho tabela */}
-    // <div>
-    //     {/* Título da tabela */}
-    //     <p>Tipos de caminhões cadastrados</p>
+//     {/* Botão cadastrar */}
+//     <button>+ Cadastrar caminhão</button>
+// </div>
 
-    //     {/* Botão cadastrar */}
-    //     <button>+ Cadastrar caminhão</button>
-    // </div>
+// {/* Títulos colunas tabela */}
+// <div>
+//     <p>Nome do tipo de caminhão</p>
+//     <p>Opções</p>
+// </div>
 
-    // {/* Títulos colunas tabela */}
-    // <div>
-    //     <p>Nome do tipo de caminhão</p>
-    //     <p>Opções</p>
-    // </div>
+// {/* Corpo da tabela */}
+// <div>
+//     {/* Mensagem caso não haja registros ou então mostrar os registros*/}
+//     {/* Mensagem */}
+//     <div>
+//     <div>Imagem</div>
+//     <p>Mensagem</p>
+//     </div>
 
-    // {/* Corpo da tabela */}
-    // <div>
-    //     {/* Mensagem caso não haja registros ou então mostrar os registros*/}
-    //     {/* Mensagem */}
-    //     <div>
-    //     <div>Imagem</div>
-    //     <p>Mensagem</p>
-    //     </div>
+//     {/* Registros tabela */}
+//     <div>
+//     {/* Nome do tipo de caminhão */}
+//     <div>
+//         <p>Romeu e julieta</p>
+//     </div>
 
-    //     {/* Registros tabela */}
-    //     <div>
-    //     {/* Nome do tipo de caminhão */}
-    //     <div>
-    //         <p>Romeu e julieta</p>
-    //     </div>
-
-    //     {/* Ícone de opções */}
-    //     <div>
-    //         <button>menu</button>
-    //         {/* Modal
-    //             Título: Opções nome caminhao
-    //             Botões:
-    //             ico-img vizualizar imagem
-    //             ico-edt Editar dados
-    //             ico-lix Excluir caminhão
-    //         */}
-    //     </div>
-    //     </div>
-    // </div>
-    // </div>
+//     {/* Ícone de opções */}
+//     <div>
+//         <button>menu</button>
+//         {/* Modal
+//             Título: Opções nome caminhao
+//             Botões:
+//             ico-img vizualizar imagem
+//             ico-edt Editar dados
+//             ico-lix Excluir caminhão
+//         */}
+//     </div>
+//     </div>
+// </div>
+// </div>

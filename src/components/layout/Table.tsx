88712d7f1cec036517cb57e4099cc8501147f2ee
Graefzera <laugraeffcorreia1821@gmail.com>
2026-02-05 17:25:35 -0300

    estilização da tabela

--- src/components/layout/Table.tsx
+++ src/components/layout/Table.tsx
@@ -2,40 +2,49 @@ export default function Table() {
   return (
     <>
       <div>
-        <div className="bg-[#1e1e1e] mt-7.5 mx-9 sm:mt-7.5 sm:mx-18 text-white montserrat rounded-sm">
+        <div className="bg-[#1e1e1e] mt-7.5 mx-9 sm:mx-18 lg:mx-36 xl:mx-72 text-white montserrat rounded-sm">
           {/* Cabeçalho da tabela */}
           <div>
             {/* Título da tabela */}
-            <div className="border-b border-b-[#2A2E35]">
-                <div className="flex justify-between items-center gap-10">
-                  <h2 className="sm:text-[16px] lg:text-[26px] p-2 m-5">
-                    Tipos de caminhões cadastrados
-                  </h2>
-                  <button className="bg-[#00A03C33] py-2 px-4 border border-[#00A03C] m-5 hover:bg-[#00A03CB2] hover:cursor-pointer rounded-sm">+ Cadastrar caminhão</button>
-                </div>
-
+            <div className="border-b border-b-[#2A2E35] ">
+              <div className="flex justify-between items-center">
+                <h2 className="sm:text-[14px] lg:text-[20px] m-4.5 ">
+                  Tipos de caminhões cadastrados
+                </h2>
+                <button className="bg-[#00A03C33] py-2 px-4 m-4.5 border border-[#00A03C]  hover:bg-[#00A03CB2] hover:cursor-pointer rounded-sm sm:text-[14px] lg:text-[16px]">
+                  + Cadastrar caminhão
+                </button>
+              </div>
             </div>
 
             {/* Títulos colunas tabela */}
-            <div className="flex justify-between py-2 px-4 border-b border-b-[#2A2E35]">
-              <p className="p-4">Nome do tipo de caminhão</p>
-              <p className="p-4">Opções</p>
+            <div className="flex justify-between items-center border-b border-b-[#2A2E35] mb-3.5">
+              <p className="sm:text-[14px] lg:text-[16px] m-4.5">Nome do tipo de caminhão</p>
+              <p className="sm:text-[14px] lg:text-[16px] m-4.5">Opções</p>
             </div>
           </div>
 
           {/* Corpo da tabela */}
-          <tbody>
+          <div className="h-100">
             {/* Se não houver registros */}
             {/* <tr>
                 <td>ico-cloud e Mensagem</td>
                 </tr> */}
 
             {/* Registros */}
-            <tr>
-              <td>Nome do caminhão</td>
-              <td>ico-menu</td>
-            </tr>
-          </tbody>
+            <div className="bg-[#2A2E35] flex justify-between items-center mx-4.5 mb-1.25 rounded-sm">
+              <p className="m-2">Nome do caminhão</p>
+              <i className="bi bi-list text-[18px] m-2 hover:text-[#00A03CB2] hover:cursor-pointer"></i>
+            </div>
+            <div className="bg-[#2A2E35] flex justify-between items-center mx-4.5 mb-1.25 rounded-sm">
+              <p className="m-2">Nome do caminhão</p>
+              <i className="bi bi-list text-[18px] m-2 hover:text-[#00A03CB2] hover:cursor-pointer"></i>
+            </div>
+            <div className="bg-[#2A2E35] flex justify-between items-center mx-4.5 mb-1.25 rounded-sm">
+              <p className="m-2">Nome do caminhão</p>
+              <i className="bi bi-list text-[18px] m-2 hover:text-[#00A03CB2] hover:cursor-pointer"></i>
+            </div>
+          </div>
         </div>
       </div>
     </>
